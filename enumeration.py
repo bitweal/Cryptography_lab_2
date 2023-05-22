2ecf7f2063feaee1e76bcdf7c86e8db3ad87dc28
Juuzou Suzuya <bitweal2@gmail.com>
2023-05-22 17:05:26 +0300

    enumeration

---
+++ enumeration.py
@@ -0,0 +1,16 @@
+﻿def discrete_logarithm_enumer(alpha, beta, p):
+    for x in range(p - 1):
+        if pow(alpha, x, p) == beta:
+            return x
+    return None
+
+p = int(input('Enter p: '))
+alpha = int(input('Enter alpha: '))
+beta = int(input('Enter beta: '))
+
+result = discrete_logarithm_enumer(alpha, beta, p)
+
+if result is not None:
+    print(f"Дискретний логарифм числа {beta} з основою {alpha} = {result}")
+else:
+    print(f"Дискретного логарифму не знайдено")
